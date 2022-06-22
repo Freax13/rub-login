e10e3d1dfdf468038d0a6bd63dabb772769738f4
Tom Dohrmann <erbse.13@gmx.de>
2022-06-22 21:08:37 +0200

    add default nixosModules with systemd service

--- flake.nix
+++ flake.nix
@@ -37,5 +37,40 @@
           inputsFrom = builtins.attrValues self.packages.${system};
           buildInputs = [ pkgs.cargo pkgs.rust-analyzer pkgs.clippy ];
         };
-      });
+      }) // {
+    nixosModules.default = { lib, pkgs, config, ... }: with lib; 
+    let
+      cfg = config.services.rub-login;
+      rub-loginBin = self.packages.${pkgs.stdenv.hostPlatform.system}.rub-login;
+    in {
+      options.services.rub-login = {
+        enable = mkEnableOption "rub-login";
+        username = mkOption {
+          type = types.str;
+        };
+        passwordFile = mkOption {
+          type = types.str;
+        };
+      };
+
+      config = mkIf cfg.enable {
+        systemd.services.rub-login = {
+          serviceConfig = {
+            Type = "oneshot";
+            ExecStart = "${rub-loginBin}/bin/rub-login login ${escapeShellArg cfg.username} ${escapeShellArg cfg.passwordFile}";
+          };
+        };
+
+        systemd.timers.rub-login = {
+          wantedBy = [ "timers.target" ];
+          after = [ "network-online.target" ];
+          timerConfig = {
+            OnActiveSec = [ "0" ];
+            OnCalendar = [ "*:0/15" ];
+            Persistent = true;
+          };
+        };
+      };
+    };
+  };
 }
